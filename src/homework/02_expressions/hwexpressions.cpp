1e0152dc1c54339551117cded38470d5e4fd8f71
AndeeNg-1 <thong.nguyen4@g.austincc.edu>
2025-02-12 03:22:29 +0000

    homework 2

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -8,6 +8,12 @@ int add_numbers(int num1, int num2)
 
 //write function code here
 
+int get_sales_tax_amount(int meal_amount, int tax_rate)
+{
+	return meal_amount * tax_rate;
+}
 
-
-
+int get_tip_amount(int meal_amount, int tip_rate)
+{
+	return meal_amount * tip_rate;
+}
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,29 @@
 ﻿//write include statements
+#include<iostream>
+#include "hwexpressions.h"
 
-
+using std::cout; using std::cin; using std::endl;
 //write namespace using statement for cout
 
 int main()
 {
+	double meal_amount, tip_rate, tip_amount, tax_amount, total;
 	
+	cout << "Enter meal amount: ";
+	cin >> meal_amount;
 
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout << "Enter tip rate (as demical): ";
+	cin >> tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout << "Meal Amount: " << meal_amount << endl;
+	cout << "Sales Tax: " << tax_amount << endl;
+	cout << "Tip Amount: " << tip_amount << endl;
+	cout << "Total: " << total << endl;
 	return 0;
 }
